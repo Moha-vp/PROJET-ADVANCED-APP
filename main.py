e43ebe285f9d7a06cc699312e32a1eea92d16fef
SidAhmedHim <155841137+SidAhmedHim@users.noreply.github.com>
2025-01-18 03:56:36 +0100

    New user And Update user function

--- main.py
+++ main.py
@@ -16,3 +16,20 @@ def find_user(users_data, user_id):
         if user["id"] == user_id:
             return user
     return None
+
+def add_new_user(users_data, user_id, name):
+    new_user = {
+        "id": user_id,
+        "name": name,
+        "history": []
+    }
+    users_data["users"].append(new_user)
+    return new_user
+
+def update_user_history(user, score, category):
+    from datetime import datetime
+    user["history"].append({
+        "date": datetime.now().strftime("%Y-%m-%d"),
+        "score": score,
+        "category": category
+    })
